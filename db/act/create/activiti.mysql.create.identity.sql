9c588359e7183e7023f43ec341207e7d3571db98
xuyonghai123 <120961361@qq.com>
2017-11-11 20:34:25 +0800

    提交

--- db/act/create/activiti.mysql.create.identity.sql
+++ db/act/create/activiti.mysql.create.identity.sql
@@ -1,46 +1,46 @@
-create table ACT_ID_GROUP (
-    ID_ varchar(64),
-    REV_ integer,
-    NAME_ varchar(255),
-    TYPE_ varchar(255),
-    primary key (ID_)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+create table act_id_group (
+    id_ varchar(64),
+    rev_ integer,
+    name_ varchar(255),
+    type_ varchar(255),
+    primary key (id_)
+) engine=innodb default charset=utf8 collate utf8_bin;
 
-create table ACT_ID_MEMBERSHIP (
-    USER_ID_ varchar(64),
-    GROUP_ID_ varchar(64),
-    primary key (USER_ID_, GROUP_ID_)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+create table act_id_membership (
+    user_id_ varchar(64),
+    group_id_ varchar(64),
+    primary key (user_id_, group_id_)
+) engine=innodb default charset=utf8 collate utf8_bin;
 
-create table ACT_ID_USER (
-    ID_ varchar(64),
-    REV_ integer,
-    FIRST_ varchar(255),
-    LAST_ varchar(255),
-    EMAIL_ varchar(255),
-    PWD_ varchar(255),
-    PICTURE_ID_ varchar(64),
-    primary key (ID_)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+create table act_id_user (
+    id_ varchar(64),
+    rev_ integer,
+    first_ varchar(255),
+    last_ varchar(255),
+    email_ varchar(255),
+    pwd_ varchar(255),
+    picture_id_ varchar(64),
+    primary key (id_)
+) engine=innodb default charset=utf8 collate utf8_bin;
 
-create table ACT_ID_INFO (
-    ID_ varchar(64),
-    REV_ integer,
-    USER_ID_ varchar(64),
-    TYPE_ varchar(64),
-    KEY_ varchar(255),
-    VALUE_ varchar(255),
-    PASSWORD_ LONGBLOB,
-    PARENT_ID_ varchar(255),
-    primary key (ID_)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+create table act_id_info (
+    id_ varchar(64),
+    rev_ integer,
+    user_id_ varchar(64),
+    type_ varchar(64),
+    key_ varchar(255),
+    value_ varchar(255),
+    password_ longblob,
+    parent_id_ varchar(255),
+    primary key (id_)
+) engine=innodb default charset=utf8 collate utf8_bin;
 
-alter table ACT_ID_MEMBERSHIP 
-    add constraint ACT_FK_MEMB_GROUP 
-    foreign key (GROUP_ID_) 
-    references ACT_ID_GROUP (ID_);
+alter table act_id_membership
+    add constraint act_fk_memb_group
+    foreign key (group_id_)
+    references act_id_group (id_);
 
-alter table ACT_ID_MEMBERSHIP 
-    add constraint ACT_FK_MEMB_USER 
-    foreign key (USER_ID_) 
-    references ACT_ID_USER (ID_);
+alter table act_id_membership
+    add constraint act_fk_memb_user
+    foreign key (user_id_)
+    references act_id_user (id_);
