1c858749ce838b2d32d5734b8547dc449fa3bf43
xuyonghai123 <120961361@qq.com>
2017-11-30 15:31:35 +0800

    zabbixUtil更新

--- src/main/java/com/thinkgem/jeesite/common/utils/ZabbixUtils.java
+++ src/main/java/com/thinkgem/jeesite/common/utils/ZabbixUtils.java
@@ -3,19 +3,20 @@ package com.thinkgem.jeesite.common.utils;
 
 import com.zabbix4j.ZabbixApi;
 import com.zabbix4j.ZabbixApiException;
-import com.zabbix4j.host.*;
+import com.zabbix4j.host.Host;
+import com.zabbix4j.host.HostGetRequest;
+import com.zabbix4j.host.HostGetResponse;
 import com.zabbix4j.item.Item;
 import com.zabbix4j.item.ItemGetRequest;
 import com.zabbix4j.item.ItemGetResponse;
-import com.zabbix4j.item.ItemObject;
-
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Thinkpad on 17/09/21.
  **/
 public class ZabbixUtils {
-
+    protected Logger logger = LoggerFactory.getLogger(getClass());
     private static final String ZBX_URL = "http://45.77.68.69:8080/zabbix/api_jsonrpc.php";
     private static final String USERNAME = "Admin";
     private static final String PASSWORD = "zabbix";
@@ -27,17 +28,20 @@ public class ZabbixUtils {
         try {
             zabbixApi.login(USERNAME,PASSWORD);
 
+
+            //获取Zabbix的item
             Item item = zabbixApi.item();
             ItemGetResponse itemGetResponse= item.get(itemGetRequest);
+            System.out.println("itemSize:"+itemGetResponse.getResult().size());
             for (int i = 0; i < itemGetResponse.getResult().size(); i++) {
-                System.out.println(itemGetResponse.getResult().get(i).getName());
+                System.out.println("item"+i+":"+itemGetResponse.getResult().get(i).getName());
             }
-            System.out.println(itemGetResponse.getResult().size());
+            //获取zabbix的host
+            System.out.println("--------------------------------");
+            Host host = zabbixApi.host();
+            HostGetResponse hostGetResponse= host.get(hostGetRequest);
+            System.out.println("host:"+hostGetResponse.getResult().get(0).getName());
 
-//            Host host = zabbixApi.host();
-//            HostGetResponse hostGetResponse= host.get(hostGetRequest);
-//            System.out.println(hostGetResponse.getResult().get(0).getName());
-//            System.out.println("--------------------------------");
         } catch (ZabbixApiException e) {
             e.printStackTrace();
         }
